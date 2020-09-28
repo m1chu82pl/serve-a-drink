9bb4b1813e9e54fa5590a909bc74f3110d36ef45
m1chu82pl <criminal.industries@gmail.com>
2020-09-28 13:16:48 +0200

    add styles in Cocktail

--- src/components/Cocktail.js
+++ src/components/Cocktail.js
@@ -1,11 +1,17 @@
 import React from "react";
-import './Cocktail.scss';
+import "./Cocktail.scss";
 
 const Cocktail = (props) => {
+  const { idDrink, strDrink, strDrinkThumb } = props.cocktail;
 
-const {idDrink, strDrink} = props.cocktail;
-
-return <div className="cocktail" onClick={ () => props.showDrink(idDrink)}>"{strDrink}" have got: {idDrink} ID</div>;
+  return (
+    <div className="cocktail--container">
+      <p className="cocktail--name" onClick={() => props.showDrink(idDrink)}>
+        "{strDrink}" have got: {idDrink} ID
+      </p>
+      <img className="cocktail--img"src={strDrinkThumb} alt="some drink"/>
+    </div>
+  );
 };
 
 export default Cocktail;
