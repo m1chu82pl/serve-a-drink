1d4015807036545d687c41c90e4d9f57dbc94c79
m1chu82pl <criminal.industries@gmail.com>
2020-09-30 08:34:58 +0200

    add background div in CocktailDetails

--- src/components/CocktailDetails.js
+++ src/components/CocktailDetails.js
@@ -4,9 +4,18 @@ import "./CocktailDetails.scss";
 const CocktailDetails = (props) => {
   const { idDrink, strDrink, strInstructions } = props.cocktailDetails;
 
+  const imgContainer = {
+    background:
+      "url(https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg) no-repeat",
+    boxShadow: "0 0 80px 80px white inset",
+    width: "600px",
+    height: "600px",
+  };
+
   // console.log(props.cocktailDetails);
   return (
     <div className="cocktailDetails">
+      <div className="cocktailDetails--image" style={imgContainer}></div>
       <h1>{strDrink}</h1>
       <h2>{idDrink}</h2>
       <h3>{strInstructions}</h3>
