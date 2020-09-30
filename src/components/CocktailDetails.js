917e58f5a99a6c034b2d755373d404d161d36032
m1chu82pl <criminal.industries@gmail.com>
2020-09-30 09:12:03 +0200

    add boxShadow

--- src/components/CocktailDetails.js
+++ src/components/CocktailDetails.js
@@ -2,24 +2,32 @@ import React from "react";
 import "./CocktailDetails.scss";
 
 const CocktailDetails = (props) => {
-  const { idDrink, strDrink, strInstructions } = props.cocktailDetails;
+  const {
+    idDrink,
+    strDrink,
+    strInstructions,
+    strDrinkThumb,
+  } = props.cocktailDetails;
 
   const imgContainer = {
-    background:
-      "url(https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg) no-repeat",
-    boxShadow: "0 0 80px 80px white inset",
-    width: "600px",
-    height: "600px",
+    background: `url(${strDrinkThumb}) no-repeat fixed center`,
+    // backgroundRepeat: "no-repeat",
+    // backgroundSize: "100% 100%",
+    // "url(https://www.thecocktaildb.com/images/media/drink/5noda61589575158.jpg) no-repeat",
+    boxShadow: "100px 25px 50px 0 white inset, -25px -25px 50px 0 white inset",
+    width: "100%",
+    height: "100%",
   };
 
   // console.log(props.cocktailDetails);
   return (
     <div className="cocktailDetails">
-      <div className="cocktailDetails--image" style={imgContainer}></div>
-      <h1>{strDrink}</h1>
-      <h2>{idDrink}</h2>
-      <h3>{strInstructions}</h3>
-      <button onClick={props.clicked}>return to list</button>
+      <div className="cocktailDetails--image" style={imgContainer}>
+        <h1 style={{ fontSize: "60px" }}>{strDrink}</h1>
+        <h2>{idDrink}</h2>
+        <h3>{strInstructions}</h3>
+        <button onClick={props.clicked}>return to list</button>
+      </div>
     </div>
   );
 };
